d70601b9ecee6e4f3c6f7f657cb315e836c45d57
TRIVAIT <dimitmapper@gmail.com>
2025-04-10 20:18:44 +0400

    Reload project

--- src/main/java/org/trivait/hammer_mod/HammerModDataGenerator.java
+++ src/main/java/org/trivait/hammer_mod/HammerModDataGenerator.java
@@ -1,4 +1,4 @@
-package org.trivait;
+package org.trivait.hammer_mod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
--- src/main/java/org/trivait/hammer_mod/mixin/ExampleMixin.java
+++ src/main/java/org/trivait/hammer_mod/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package org.trivait.mixin;
+package org.trivait.hammer_mod.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
